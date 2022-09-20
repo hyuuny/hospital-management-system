0655c927dbbad0f457b58d6150151e8064970aff
hyuuny <shyune@knou.ac.kr>
2022-09-20 21:53:08 +0900

    feat: 1. 웹 프로젝트 구성

---
+++ src/main/kotlin/com/hyuuny/hospitalmanagementsystem/HospitalManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.hyuuny.hospitalmanagementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HospitalManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<HospitalManagementSystemApplication>(*args)
+}
