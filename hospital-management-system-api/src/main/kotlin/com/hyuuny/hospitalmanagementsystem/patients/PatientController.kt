ff245d0a59fde236cf36da11925d684f534f5651
hyuuny <shyune@knou.ac.kr>
2022-09-21 18:31:46 +0900

    feat: PatientController `환자 상세 조회` 구현 및 PatientResourceAssembler 추가
    PatientResourceAssembler로 RESTful 응답 처리

--- hospital-management-system-api/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/patients/PatientController.kt
+++ hospital-management-system-api/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/patients/PatientController.kt
@@ -1,37 +1,47 @@
 package com.hyuuny.hospitalmanagementsystem.patients
 
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.RepresentationModelAssembler
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping(path = ["/api/v1/patients"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @RestController
 class PatientController(
     private val patientService: PatientService,
+    private val patientResourceAssembler: PatientResourceAssembler,
 ) {
-가
-    @PostMapping
-    fun createPatient() {
-
-    }
-
-    @PutMapping("/{id}")
-    fun updatePatient() {
-
-    }
-
-    @DeleteMapping("/{id}")
-    fun deletePatient() {
-
-    }
 
     @GetMapping("/{id}")
-    fun getPatient() {
-
+    fun getPatient(@PathVariable id: Long): EntityModel<PatientResponse> {
+        val foundPatient = patientService.getPatient(id)
+        return patientResourceAssembler.toModel(foundPatient)
     }
 
     @GetMapping
     fun retrievePatients() {
 
+    }
+
+    @Component
+    companion object PatientResourceAssembler :
+        RepresentationModelAssembler<PatientResponse, EntityModel<PatientResponse>> {
+
+        override fun toModel(entity: PatientResponse): EntityModel<PatientResponse> {
+            return EntityModel.of(
+                entity,
+                WebMvcLinkBuilder.linkTo(
+                    WebMvcLinkBuilder.methodOn(PatientController::class.java)
+                        .getPatient(entity.id)
+                )
+                    .withSelfRel()
+            )
+        }
     }
 
 
