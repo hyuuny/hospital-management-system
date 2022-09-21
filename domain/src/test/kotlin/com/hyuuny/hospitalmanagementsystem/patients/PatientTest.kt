d6d1a36e9416d9f5c01b2df79839e72fb7ef9acc
hyuuny <shyune@knou.ac.kr>
2022-09-21 11:24:07 +0900

    test: PatientTest `환자 객체 생성` 테스트 추가
    테스트에서 사용하기 위한 createPatient 객체 생성 메서드 추가

---
+++ domain/src/test/kotlin/com/hyuuny/hospitalmanagementsystem/patients/PatientTest.kt
@@ -0,0 +1,43 @@
+package com.hyuuny.hospitalmanagementsystem.patients
+
+import com.hyuuny.hospitalmanagementsystem.visits.Visit
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PatientTest {
+
+    @Test
+    fun `환자 객체 생성`() {
+        val expectedHospitalId = 1L
+        val expectedName = "김환자"
+        val expectedGender = "M"
+        val expectedBirthDay = "1993-01-01"
+        val expectedMobilePhoneNumber = "010-1324-1234"
+
+        val newPatient = createPatient()
+        assertThat(newPatient.hospitalId).isEqualTo(expectedHospitalId)
+        assertThat(newPatient.name).isEqualTo(expectedName)
+        assertThat(newPatient.gender.name).isEqualTo(expectedGender)
+        assertThat(newPatient.birthDay).isEqualTo(expectedBirthDay)
+        assertThat(newPatient.mobilePhoneNumber).isEqualTo(expectedMobilePhoneNumber)
+        assertThat(newPatient.visits).isEmpty()
+        assertThat(newPatient.registerNo).isNotNull
+    }
+
+    private fun createPatient(
+        hospitalId: Long = 1,
+        name: String = "김환자",
+        gender: String = "M",
+        brithDay: String = "1993-01-01",
+        mobilePhoneNumber: String = "010-1324-1234",
+        visits: MutableList<Visit> = mutableListOf(),
+    ): Patient = Patient.create(
+        hospitalId = hospitalId,
+        name = name,
+        gender = gender,
+        birthDay = brithDay,
+        mobilePhoneNumber = mobilePhoneNumber,
+        visits = visits,
+    )
+
+}
