bec0c968bf8bfc1a11df91e7de1406d3d5736787
hyuuny <shyune@knou.ac.kr>
2022-09-22 12:35:16 +0900

    feat: VisitController `환자 방문 기록 상세 조회` 구현 및 VisitResourceAssembler 추가
    VisitResourceAssembler로 RESTful 응답 처리

--- hospital-management-system-api/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/visits/VisitController.kt
+++ hospital-management-system-api/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/visits/VisitController.kt
@@ -1,32 +1,48 @@
 package com.hyuuny.hospitalmanagementsystem.visits
 
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.RepresentationModelAssembler
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping(path = ["/api/v1/{patientId}}/visits"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(
+    path = ["/api/v1/{patientId}/visits"],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+)
 @RestController
 class VisitController(
     private val visitService: VisitService,
+    private val visitResourceAssembler: VisitResourceAssembler,
 ) {
 
-    @PostMapping
-    fun createVisit() {
-
-    }
-
-    @PutMapping("/{id}")
-    fun updateVisit() {
-
-    }
-
-    @DeleteMapping("/{id}")
-    fun deleteVisit() {
-
-    }
-
     @GetMapping("/{id}")
-    fun getVisit() {
+    fun getVisit(
+        @PathVariable patientId: Long,
+        @PathVariable id: Long,
+    ): EntityModel<VisitResponse> {
+        val foundVisit = visitService.getVisit(id)
+        return visitResourceAssembler.toModel(foundVisit)
+    }
 
+    @Component
+    class VisitResourceAssembler :
+        RepresentationModelAssembler<VisitResponse, EntityModel<VisitResponse>> {
+
+        override fun toModel(entity: VisitResponse): EntityModel<VisitResponse> {
+            return EntityModel.of(
+                entity,
+                WebMvcLinkBuilder.linkTo(
+                    WebMvcLinkBuilder.methodOn(VisitController::class.java)
+                        .getVisit(entity.patientId, entity.id)
+                )
+                    .withSelfRel()
+            )
+        }
     }
 
 }
