237cfdcc0c26182c92ed89f086a8f9dbb8c37978
hyuuny <shyune@knou.ac.kr>
2022-09-22 14:55:25 +0900

    feat: PatientRepositoryImpl `retrievePatients` 메서드 where절 및 최근 방문 조회되도록 추가
    환자이름, 환자등록번호, 생년월일로 검색되도록 추가

--- infrastructure/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/patients/PatientRepositoryImpl.kt
+++ infrastructure/src/main/kotlin/com/hyuuny/hospitalmanagementsystem/patients/PatientRepositoryImpl.kt
@@ -3,16 +3,21 @@ package com.hyuuny.hospitalmanagementsystem.patients
 import com.hyuuny.hospitalmanagementsystem.patients.QPatient.patient
 import com.hyuuny.hospitalmanagementsystem.support.CustomQueryDslRepository
 import com.hyuuny.hospitalmanagementsystem.visits.QVisit.visit
+import com.querydsl.core.types.ExpressionUtils
 import com.querydsl.core.types.Projections.fields
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import org.springframework.util.ObjectUtils.isEmpty
 
 class PatientRepositoryImpl(
     private val queryFactory: JPAQueryFactory,
 ) : CustomQueryDslRepository(Patient::class.java), PatientRepositoryCustom {
 
-    override fun retrievePatients(pageable: Pageable): PageImpl<SearchedPatientListing> {
+    override fun retrievePatients(
+        searchCondition: PatientSearchCondition,
+        pageable: Pageable,
+    ): PageImpl<SearchedPatientListing> {
         return applyPageImpl(
             pageable, queryFactory
                 .select(
@@ -25,12 +30,26 @@ class PatientRepositoryImpl(
                         patient.gender,
                         patient.birthDay,
                         patient.mobilePhoneNumber,
-                        visit.receptionDateTime,
+                        ExpressionUtils.`as`(visit.receptionDateTime.max(), "receptionDateTime"),
                     )
                 )
                 .from(patient)
                 .leftJoin(visit).on(visit.patient.id.eq(patient.id))
+                .where(
+                    nameEq(searchCondition.name),
+                    registerNoEq(searchCondition.registerNo),
+                    birthDayEq(searchCondition.birthDay),
+                )
         )
 
     }
+
+    private fun nameEq(name: String?) = if (isEmpty(name)) null else patient.name.eq(name)
+
+    private fun registerNoEq(registerNo: String?) =
+        if (isEmpty(registerNo)) null else patient.registerNo.eq(registerNo)
+
+    private fun birthDayEq(birthDay: String?) =
+        if (isEmpty(birthDay)) null else patient.birthDay.eq(birthDay)
+
 }
